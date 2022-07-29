5e7ecfb8b304c226bef2346e66c5daa3c6795d27
hlohi <37447558+hlohi@users.noreply.github.com>
2022-07-29 14:18:32 +0200

    Add files via upload

---
+++ Controller/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using ContosoCrafts.WebSite.Services;
+using ContosoCraftsWebsite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCraftsWebsite.Controller
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+        
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get([FromQuery] string Product,[FromQuery] int Rating)
+        {
+            return ProductService.GetProducts();
+        }
+
+    }
+}
